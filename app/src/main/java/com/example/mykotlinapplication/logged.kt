cfe847beb315c7aaf93b04c9d673117664a9d1a6
denis <denis.escobar@institutvidreres.cat>
2021-10-27 19:46:23 +0200

    2.0

--- app/src/main/java/com/example/mykotlinapplication/logged.kt
+++ app/src/main/java/com/example/mykotlinapplication/logged.kt
@@ -2,23 +2,31 @@ package com.example.mykotlinapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.mykotlinapplication.databinding.ActivityLoggedBinding
 
 class logged : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_logged)
+//        setContentView(R.layout.activity_logged)
+        val binding = DataBindingUtil.setContentView<ActivityLoggedBinding>(this,
+            R.layout.activity_logged)
 
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this,navController)
 
-      //  NavigationUI.setupWithNavController(binding.navView, navController)
+
+        drawerLayout = binding.drawer
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
