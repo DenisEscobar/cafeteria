cf65ceec5167cbf8dc8936c350b1c12650731463
Denis <Denis.escobar@institutvidreres.cat>
2021-11-09 21:51:37 +0100

    todo en carpeta i data base

--- app/src/main/java/com/example/mykotlinapplication/Principal/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapplication/Principal/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.example.mykotlinapplication.DataBase.Register2
 import com.example.mykotlinapplication.R
 import com.example.mykotlinapplication.databinding.ActivityMainBinding
 
--- app/src/main/java/com/example/mykotlinapplication/Principal/Register2.kt
+++ app/src/main/java/com/example/mykotlinapplication/Principal/Register2.kt
@@ -1,4 +1,4 @@
-package com.example.mykotlinapplication.DataBase
+package com.example.mykotlinapplication.Principal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
