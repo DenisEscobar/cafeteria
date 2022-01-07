735e762458afad932b747f7cd1f0edddbc0b5227
DenisEscobar <92932342+DenisEscobar@users.noreply.github.com>
2022-01-07 12:57:02 +0100

    Update ComandaDatabase.kt

--- app/src/main/java/com/example/mykotlinapplication/DataBase/ComandaDatabase.kt
+++ app/src/main/java/com/example/mykotlinapplication/DataBase/ComandaDatabase.kt
@@ -8,7 +8,7 @@ import androidx.room.RoomDatabase
 import com.example.mykotlinapplication.RoomViewModel
 import com.example.mykotlinapplication.RoomViewModelFactory
 
-@Database(entities = [Comanda::class, log::class, platos::class], version = 3, exportSchema = false)
+@Database(entities = [Comanda::class, log::class, platos::class], version = 4, exportSchema = false)
 abstract class ComandaDatabase : RoomDatabase() {
     abstract val comandaDatabaseDao: ComandaDatabaseDao
 
@@ -27,12 +27,52 @@ abstract class ComandaDatabase : RoomDatabase() {
                         ComandaDatabase::class.java,
                         "sleep_history_database"
                     )
+                    .addCallback(object : Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            Thread(Runnable { prepopulateDb(getInstance(context)) }).start()
+                        }
+                    })
                         .fallbackToDestructiveMigration()
                         .allowMainThreadQueries()
                         .build()
                     INSTANCE = instance
                 }
                 return instance
+            }
+        }
+        private fun prepopulateDb(db: ComandaDatabase) {
+            GlobalScope.launch {
+                val plat=platos()
+                plat.NomPlato="coca-cola"
+                plat.PrecioPlato="2.5"
+                plat.DescripcioPlato="cola"
+                plat.CategoriaPlato="beguda"
+                db.comandaDatabaseDao.insertplat(plat)
+                
+                plat.NomPlato="fanta llimona"
+                plat.PrecioPlato="2"
+                plat.DescripcioPlato="llimona"
+                plat.CategoriaPlato="beguda"
+                db.comandaDatabaseDao.insertplat(plat)
+                
+                plat.NomPlato="fanta taronga"
+                plat.PrecioPlato="2"
+                plat.DescripcioPlato="taronga"
+                plat.CategoriaPlato="beguda"
+                db.comandaDatabaseDao.insertplat(plat)
+
+                plat.NomPlato="entrepan fuet"
+                plat.PrecioPlato="3"
+                plat.DescripcioPlato="fuet"
+                plat.CategoriaPlato="entrepan"
+                db.comandaDatabaseDao.insertplat(plat)
+
+                plat.NomPlato="cafe"
+                plat.PrecioPlato="1.5"
+                plat.DescripcioPlato="cafe"
+                plat.CategoriaPlato="postre"
+                db.comandaDatabaseDao.insertplat(plat)
             }
         }
     }
