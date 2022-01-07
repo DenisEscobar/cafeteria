caa89119822e2d1e2e0f5d829c1d7a90525604b1
DenisEscobar <92932342+DenisEscobar@users.noreply.github.com>
2022-01-07 12:59:34 +0100

    Update platofav.kt

--- app/src/main/java/com/example/mykotlinapplication/DataBase/platofav.kt
+++ app/src/main/java/com/example/mykotlinapplication/DataBase/platofav.kt
@@ -1,1 +1,16 @@
+package com.example.mykotlinapplication.DataBase
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "platosfav")
+data class platofav(
+    @ColumnInfo(name = "nomPlato")
+    var NomPlato: String? = null,
+    @ColumnInfo(name = "nomUsuari")
+    var DescripcioPlato: String? = null,
+    @ColumnInfo(name = "fav")
+    var PrecioPlato: String? = null,
+    )
